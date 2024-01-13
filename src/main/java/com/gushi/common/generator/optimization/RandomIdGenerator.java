21498f832cc3e04ea75882d14fe2063b58f55087
Gushi1912 <18291855713@163.com>
2024-01-13 17:31:38 +0800

    feat:添加方法和类的注释说明

--- src/main/java/com/gushi/common/generator/optimization/RandomIdGenerator.java
+++ src/main/java/com/gushi/common/generator/optimization/RandomIdGenerator.java
@@ -9,6 +9,7 @@ import java.net.UnknownHostException;
 import java.util.Random;
 
 /**
+ * 一个用来生成随机ID的生成器
  * @Author Gushiyang
  * @Version 1.0.0
  * @Time 2024/1/13 16:22
@@ -18,8 +19,9 @@ public class RandomIdGenerator implements  LogTraceIdGenerator{
     private static final Logger logger = LoggerFactory.getLogger(RandomIdGenerator.class);
 
     /**
-     *
-     * @return
+     * 生成一段随机字符串，字符串是由主机名的最后一个字段，当前时间戳以及一个随机字符串组合成的，
+     * 只有在及其特殊的情况下不唯一，比如机器时钟回拨且生成同样一段随机字符串且主机名相同的情况下
+     * @return 一个随机ID
      */
     @Override
     public String generate() {
@@ -31,8 +33,8 @@ public class RandomIdGenerator implements  LogTraceIdGenerator{
 
 
     /**
-     *
-     * @return
+     * 获取当前主机名的最后一个字段的名称
+     * @return 主机名的最后一个字段名
      */
     private String getLastFiledOfHostName() {
         String subStrOfHostName = null;
@@ -47,9 +49,9 @@ public class RandomIdGenerator implements  LogTraceIdGenerator{
     }
 
     /**
-     *
-     * @param hostname
-     * @return
+     *  根据.分割主机名，并获取最后一个字段名称
+     * @param hostname 当前机器的主机名
+     * @return 主机名的最后一个字段
      */
     @VisibleForTesting
     protected String getLastSubStrSplittedByDot(String hostname) {
@@ -59,9 +61,9 @@ public class RandomIdGenerator implements  LogTraceIdGenerator{
     }
 
     /**
-     *
-     * @param length
-     * @return
+     * 生成一段{@length}长度的随机字符串，这个随机字符串由小写字母，大写字母以及数字组成
+     * @param length 字符串长度，不能小于0
+     * @return 长度为{@Length}的随机字符串
      */
     @VisibleForTesting
     protected  String generateRandomAlphameric(int length) {
